0028a59f747c0e991af03764207a213d72a60c7c
unknown <ariq.dz@gmail.com>
2022-07-04 21:03:39 +0700

    first commit

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+use App\Transaction;
+use App\TransactionDetail;
+
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $customer = User::count();
+        $revenue = Transaction::sum('total_price');
+        $transaction = Transaction::count();
+
+        $transactions = TransactionDetail::with(['transaction.user','product.galleries'])
+                            ->whereHas('product',function($product){
+                                $product->where('users_id',Auth::user()->id);
+                            });
+
+
+        return view('pages.admin.dashboard',[
+            'customer' => $customer,
+            'revenue' => $revenue,
+            'transaction_count' => $transactions->count(),
+            'transaction_data' => $transactions->get(),
+            'transaction' => $transaction
+        ]);
+    }
+}
---
+++ resources/views/pages/admin/dashboard.blade.php
@@ -0,0 +1,83 @@
+
+@extends('layouts.admin')
+
+
+@section('title')
+    Store Dashboard
+@endsection
+
+
+@section('content')
+{{-- Section Content  --}}
+  
+          <div
+            class="section-content section-dashboard-home"
+            data-aos="fade-up"
+          >
+            <div class="container-fluid">
+              <div class="dashboard-heading">
+                <h2 class="dashboard-title">Admin Dashboard</h2>
+                <p class="dashboard-subtitle">
+                  Admin Irvianda Bakery Panel
+                </p>
+              </div>
+              <div class="dashboard-content">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="card mb-2">
+                      <div class="card-body">
+                        <div class="dashboard-card-title">Pelanggan</div>
+                        <div class="dashboard-card-subtitle">{{ $customer }}</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-2">
+                      <div class="card-body">
+                        <div class="dashboard-card-title">Pendapatan</div>
+                        <div class="dashboard-card-subtitle"> Rp{{ $revenue }}</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-2">
+                      <div class="card-body">
+                        <div class="dashboard-card-title">Transasksi</div>
+                        <div class="dashboard-card-subtitle">{{ $transaction }}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row mt-3">
+                  <div class="col-12 mt-2">
+                    <h5 class="mb-3">Daftar Transaksi</h5>
+                    @foreach ($transaction_data as $transaction)
+                        <a
+                          href="{{route('dashboard-transaction-details',$transaction->id)}}"
+                          class="card card-list d-block"
+                        >
+                          <div class="card-body">
+                            <div class="row">
+                              <div class="col-md-1">
+                                <img src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}" class="w-75">
+                                
+                              </div>
+                              <div class="col-md-4">{{  $transaction->product->name ?? ''}}</div>
+                              <div class="col-md-3">{{  $transaction->transaction->user->name ?? ''}}</div>
+                              <div class="col-md-3">{{  $transaction->created_at ?? ''}}</div>
+                              <div class="col-md-1 d-none d-md-block">
+                                <img
+                                  src="/images/dashboard-arrow-right.svg"
+                                  alt=""
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </a>
+                    @endforeach                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+@endsection
