11c53bc25f4c0597522b3464d463d6bd8b00b5bd
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-08-12 21:28:28 +0700

    update

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -140,7 +140,7 @@
                           <div class="card-body">
                             <div class="row">
                               <div class="col-md-1">
-                                <img src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}" class="w-20">
+                                <img src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}" class="w-75">
                                 
                               </div>
                               <div class="col-md-1">{{  $transaction->price ?? ''  }}</div>
